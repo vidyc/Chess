68ff54f19356662df35b7c62746f2749cd7c5f42
vidyc <adrimart00@hotmail.es>
2020-09-11 02:19:38 +0200

    Create bocetoAjedrez.cc

---
+++ bocetoAjedrez.cc
@@ -0,0 +1,95 @@
+
+struct piece {
+	bool empty;
+	char idn;
+	int rank;
+	char file;
+}
+
+struct move {
+	piece origin, destination;
+	bool castling;
+	bool capture;
+	bool check;
+	bool mate;
+	char promotion; // (N, B, R, Q, -)
+	string notation;
+	
+	move(piece o, piece d, bool cast, bool capt, bool check, bool m, char prom) {
+		origin = o;
+		destination = d;
+		castling = cast;
+		capture = capt;
+		this -> check = check;
+		mate = m;
+		promotion = prom;
+	}	
+}
+
+vector<move> calculateLegalMoves() {
+	//sabemos la "x" y la "y"
+	vector<moves> legalMoves;
+
+	if (white) {
+		if (board[x][y].idn == "P") {
+
+		} else if (board[x][y].idn == "K") {
+
+		} else if (board[x][y].idn == "N") {
+
+		} else if (board[x][y].idn == "B") {
+
+			bool upLeft = 1, upRight = 1, downLeft = 1, downRight = 1;
+
+			for (int i = 0; i < 8 and (upLeft or upRight or downLeft or downRight); i++) {
+				if (upLeft) {
+					piece m = board[x - i][y - i];
+					if (m.empty) {
+						legalMoves.pushback(move(board[x][y], m, 0, 0, 0, 0, '-'));
+					} else if (m.idn >= "a" and m.idn <= "z") {
+						legalMoves.pushback(/**/);
+						upLeft = 0;
+					} else upLeft = 0;
+				}
+
+				if (upRight) {
+					piece m = board[x + i][y - i];
+					if (m.empty) {
+						legalMoves.pushback(/* me falta definir lo que es un movimiento todavia*/)
+					} else if (m.idn >= "a" and m.idn <= "z") {
+						legalMoves.pushback(/**/);
+						upRight = 0;
+					} else upRight = 0;
+				}
+
+				if (downLeft) {
+					piece m = board[x - i][y + i];
+					if (m.empty) {
+						legalMoves.pushback(/* me falta definir lo que es un movimiento todavia*/)
+					} else if (m.idn >= "a" and m.idn <= "z") {
+						legalMoves.pushback(/**/);
+						downLeft = 0;
+					} else downLeft = 0;
+				}
+
+				if (downRight) {
+					piece m = board[x + i][y + i];
+					if (m.empty) {
+						legalMoves.pushback(/* me falta definir lo que es un movimiento todavia*/)
+					} else if (m.idn >= "a" and m.idn <= "z") {
+						legalMoves.pushback(/**/);
+						downRight = 0;
+					} else downRight = 0;
+				}
+			} 
+
+		} else if (board[x][y].idn == "R") {
+
+		} else if (board[x][y].idn == "Q") {
+
+		} 
+	
+	} else {
+
+	}
+}
